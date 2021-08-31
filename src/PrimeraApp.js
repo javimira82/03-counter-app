abe42ac373da405450c4f21ec1c0dab34949db6f
JaviMira <javimira82@gmail.com>
2021-08-30 19:35:42 -0600

    Creacion de index.css y PrimeraApp.js

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const PrimeraApp = () => {
+  return (
+    <>
+      //forma corta de un Fragment
+      <h1>Hola Mundo</h1>
+      <p>Mi Primera aplicacion</p>
+    </>
+  );
+};
+
+export default PrimeraApp;
